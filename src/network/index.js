e13ba02bba6a6846b9ba1b68c8fa89681b0c7896
a-layfolk <1808150553@qq.com>
2021-02-28 18:36:05 +0800

    [feat] Add the index.js of network

---
+++ src/network/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// 配置请求的根路径
+axios.defaults.baseURL = "http://localhost:8000/admin/";
+
+// 配置请求拦截器
+axios.interceptors.request.use(config => {
+  // 为请求头对象， 添加 Token 验证的 Authorization 字段
+  const tokenStr = window.sessionStorage.getItem("token");
+  if (tokenStr) {
+    config.headers.Authorization = tokenStr;
+  }
+  return config;
+});
+
+export default axios;

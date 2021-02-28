0b828fc6c54115c7a0fc0334e7da4931ed19ed6f
a-layfolk <1808150553@qq.com>
2021-02-28 18:23:52 +0800

    [feat] Add the elementary file

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="nav">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import "./assets/css/normalize.css";
+@import "./assets/css/changedElement.css";
+</style>

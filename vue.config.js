77d33ad3ede567f86e3358e778007e1794d11736
a-layfolk <1808150553@qq.com>
2021-08-20 11:32:38 +0800

    [chore] add the compression-webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,21 @@
+const compressionPlugin = require("compression-webpack-plugin");
 module.exports = {
   publicPath: "/",
   outputDir: "dist",
   indexPath: "index.html",
   devServer: {
     port: 9000
+  },
+  // 配置 gzip 压缩
+  configureWebpack: () => {
+    return {
+      plugins: [
+        new compressionPlugin({
+          test: /\.js$|\.html$|\.css/,
+          threshold: 10240, // 超过 10kb 则打包为 gzip
+          deleteOriginalAssets: false // 是否删除源文件
+        })
+      ]
+    };
   }
-}
+};

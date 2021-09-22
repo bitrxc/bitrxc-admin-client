70f8f0f430aa9cbb0a98ca056b0399cefaba15ae
a-layfolk <1808150553@qq.com>
2021-09-22 19:21:13 +0800

    [refactor] Refactoring page styles

---
+++ src/components/Home.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="basic-layout">
+    <div :class="['nav-side', unfold ? 'unfold' : '']"></div>
+    <div :class="['main-page', unfold ? 'unfold' : '']">
+      <div class="top-menu">
+        <div class="left">
+          <img :src="unfoldUrl" @click="unfold = !unfold" />
+          <span>睿信社区管理系统后台</span>
+        </div>
+        <div class="right">
+          <el-button size="small">登出</el-button>
+        </div>
+      </div>
+      <div class="wrapper">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'Home',
+  setup () {
+    const unfoldUrl = ref(require('../assets/img/unfold.png'))
+    const unfold = ref(false)
+
+    return {
+      unfoldUrl,
+      unfold
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.basic-layout {
+  height: 100vh;
+  .nav-side {
+    position: fixed;
+    height: 100%;
+    width: 0;
+    background-image: linear-gradient(rgb(76, 150, 185), rgb(167, 171, 172));
+    transition: width .5s;
+    &.unfold {
+      width: 100px;
+    }
+  }
+  .main-page {
+    height: 100%;
+    margin-left: 0;
+    background-color: #eef0f3;
+    transition: margin-left .5s;
+    &.unfold {
+      margin-left: 100px;
+    }
+    .top-menu {
+      height: 40px;
+      padding: 0 20px;
+      background-color: #fff;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .left {
+      display: flex;
+      align-items: center;
+        img {
+          height: 30px;
+          width: 30px;
+          margin: 5px;
+        }
+        span {
+          color: #4e97b9;
+        }
+      }
+      .right {
+        .el-button {
+          color: #fff;
+          background-color: #4e97b9;
+        }
+      }
+    }
+    .wrapper {
+      min-height: calc(100vh - 100px);
+      margin: 20px;
+      background-color: #fff;
+    }
+  }
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,20 @@
 <template>
   <main>
     <aside>
+      <div>北京理工大学</div>
       <div>
-        <span>北京理工大学</span>
-        <div class="top baseline"><span></span></div>
-      </div>
-      <div>
-        <span>
-          睿信社区管理系统
-        </span>
-        <div class="bottom baseline">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+        <span>睿信社区管理系统</span>
+        <div class="baseline"><span></span><span></span><span></span></div>
       </div>
     </aside>
     <section>
       <div id="form">
         <div>管理员登入</div>
-        <el-input
-          placeholder="请输入用户名"
-          v-model="loginData.username"
-          class="input"
-        >
-          <template #prefix>
-            <i class="el-icon-user"></i>
-          </template>
+        <el-input placeholder="请输入用户名"  v-model="loginData.username"  class="input">
+          <template #prefix><i class="el-icon-user"></i></template>
         </el-input>
-        <el-input
-          type="password"
-          placeholder="请输入密码"
-          v-model="loginData.password"
-          class="input"
-        >
-          <template #prefix>
-            <i class="el-icon-lock"></i>
-          </template>
+        <el-input type="password" placeholder="请输入密码"  v-model="loginData.password"  class="input">
+          <template #prefix><i class="el-icon-lock"></i></template>
         </el-input>
         <button @click="handleLogin">登录</button>
       </div>
@@ -58,84 +36,79 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 main {
   display: flex;
   height: 100vh;
+  aside {
+    display: none;
+
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 60px;
+    color: #fff;
+
+    background-image: linear-gradient(rgb(76, 150, 185), rgb(167, 171, 172));
+
+    div:first-child {
+      font-size: 25px;
+    }
+    div:last-child {
+      font-size: 60px;
+      .baseline {
+        display: flex;
+        span {
+          display: block;
+          width: 100px;
+          height: 10px;
+          background-color: #fff;
+          margin-top: 10px;
+          margin-right: 20px;
+          opacity: 0.5;
+        }
+      }
+    }
+  }
+  section {
+    position: relative;
+    width: 100%;
+    background-color: #fff;
+
+    #form {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      text-align: center;
+
+      div {
+        float: right;
+        margin: 50px 0;
+        color: #4e97b9;
+        font-size: 25px;
+        font-weight: bold;
+      }
+
+      .input {
+        font-size: 16px;
+        margin: 10px 0;
+      }
+
+      button {
+        width: 100px;
+        height: 40px;
+        margin: 50px;
+        border: none;
+        border-radius: 20px;
+        outline: none;
+        color: #fff;
+        font-size: 20px;
+        background-color: #4e97b9;
+      }
+    }
+  }
 }
 
-/* aside 部分 */
-main aside {
-  display: none;
-
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 60px;
-  color: #fff;
-
-  background: url("../assets/img/background.png") repeat;
-}
-main aside div:first-child {
-  font-size: 25px;
-}
-main aside div:last-child {
-  font-size: 60px;
-}
-main aside div .baseline {
-  display: flex;
-}
-main aside div .baseline span {
-  display: block;
-  width: 80px;
-  height: 8px;
-  background-color: #fff;
-}
-.top span {
-  margin: 10px 0;
-}
-.bottom span {
-  margin: 15px 30px;
-  opacity: 0.5;
-}
-.bottom span:first-child {
-  opacity: 1;
-}
-
-/* section 部分 */
-main section {
-  position: relative;
-  width: 100%;
-  background-color: #fff;
-}
-main section #form {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-}
-main section #form > div {
-  float: right;
-  margin: 50px 0;
-  color: #078aae;
-  font-size: 25px;
-  font-weight: bold;
-}
-main section #form .input {
-  font-size: 16px;
-  margin: 10px 0;
-}
-main section #form button {
-  width: 100px;
-  height: 40px;
-  margin: 50px;
-  border: none;
-  border-radius: 20px;
-  outline: none;
-  color: #fff;
-  font-size: 20px;
-  background-color: #078aae;
-}
 @media screen and (min-width: 992px) {
   main aside {
     display: flex;

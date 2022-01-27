5b15a09a1f519abe3d7966a241d208f7335d59cd
LDY <yddy1100118@163.com>
2022-01-27 22:59:35 +0800

    [refactor] Fixed the problem of frequently sending requests to the back end

--- src/views/admin/AppointmentManage.vue
+++ src/views/admin/AppointmentManage.vue
@@ -23,30 +23,13 @@
     <div class="item table-header">周五</div>
     <div class="item table-header">周六</div>
     <div class="item table-header">周日</div>
-    <div class="item table-header">1</div>
-    <div class="item line-limit-length"
-      v-for="item in roomWeekArr[0]"
-      :key="item.id"
-    >
-      <div class="chosen" v-if="item.id!=null" @click="handleClick(item.id)">
-        租借人：{{item.username}}<br/>
-        学号：{{item.schoolId}}<br/>
-        备注：{{item.userNote}}<br/>
-        值班人：{{item.conductor}}<br/>
-        日期：{{item.execDate}}<br/>
-        <div v-if="item.status==='executing'">使用中</div>
-        <div v-else-if="item.status==='receive'">审批通过</div>
-        <div v-else-if="item.status==='rejected'">审批拒绝</div>
-        <div v-else>待审批</div>
-      </div>
-    </div>
-
-    <div class="item table-header">2</div>
-    <div class="item line-limit-length"
-      v-for="item in roomWeekArr[1]"
-      :key="item.id"
+    <template v-for="i in 6" :key="i">
+      <div class="item table-header">{{i}}</div>
+      <div class="item line-limit-length"
+        v-for="item in roomWeekArr[i-1]"
+        :key="item"
       >
-        <div class="chosen" v-if="item.id!=null" @click="handleClick(item.id, item.status)">
+        <div class="chosen" v-if="item.id!=null" @click="handleClick(item.id)">
           租借人：{{item.username}}<br/>
           学号：{{item.schoolId}}<br/>
           备注：{{item.userNote}}<br/>
@@ -57,79 +40,8 @@
           <div v-else-if="item.status==='rejected'">审批拒绝</div>
           <div v-else>待审批</div>
         </div>
-    </div>
-
-    <div class="item table-header">3</div>
-    <div class="item line-limit-length"
-      v-for="item in roomWeekArr[2]"
-      :key="item.id"
-    >
-      <div class="chosen" v-if="item.id!=null" @click="handleClick(item.id, item.status)">
-        租借人：{{item.username}}<br/>
-        学号：{{item.schoolId}}<br/>
-        备注：{{item.userNote}}<br/>
-        值班人：{{item.conductor}}<br/>
-        日期：{{item.execDate}}<br/>
-        <div v-if="item.status==='executing'">使用中</div>
-        <div v-else-if="item.status==='receive'">审批通过</div>
-        <div v-else-if="item.status==='rejected'">审批拒绝</div>
-        <div v-else>待审批</div>
       </div>
-    </div>
-
-    <div class="item table-header">4</div>
-    <div class="item line-limit-length"
-      v-for="item in roomWeekArr[3]"
-      :key="item.id"
-    >
-      <div class="chosen" v-if="item.id!=null" @click="handleClick(item.id, item.status)">
-        租借人：{{item.username}}<br/>
-        学号：{{item.schoolId}}<br/>
-        备注：{{item.userNote}}<br/>
-        值班人：{{item.conductor}}<br/>
-        日期：{{item.execDate}}<br/>>
-        <div v-if="item.status==='executing'">使用中</div>
-        <div v-else-if="item.status==='receive'">审批通过</div>
-        <div v-else-if="item.status==='rejected'">审批拒绝</div>
-        <div v-else>待审批</div>
-      </div>
-    </div>
-
-    <div class="item table-header">5</div>
-    <div class="item line-limit-length"
-      v-for="item in roomWeekArr[4]"
-      :key="item.id"
-    >
-      <div class="chosen" v-if="item.id!=null" @click="handleClick(item.id, item.status)">
-        租借人：{{item.username}}<br/>
-        学号：{{item.schoolId}}<br/>
-        备注：{{item.userNote}}<br/>
-        值班人：{{item.conductor}}<br/>
-        日期：{{item.execDate}}<br/>
-        <div v-if="item.status==='executing'">使用中</div>
-        <div v-else-if="item.status==='receive'">审批通过</div>
-        <div v-else-if="item.status==='rejected'">审批拒绝</div>
-        <div v-else>待审批</div>
-      </div>
-    </div>
-
-    <div class="item table-header">6</div>
-    <div class="item line-limit-length"
-      v-for="item in roomWeekArr[5]"
-      :key="item.id"
-    >
-      <div class="chosen" v-if="item.id!=null" @click="handleClick(item.id, item.status)">
-        租借人：{{item.username}}<br/>
-        学号：{{item.schoolId}}<br/>
-        备注：{{item.userNote}}<br/>
-        值班人：{{item.conductor}}<br/>
-        日期：{{item.execDate}}<br/>
-        <div v-if="item.status==='executing'">使用中</div>
-        <div v-else-if="item.status==='receive'">审批通过</div>
-        <div v-else-if="item.status==='rejected'">审批拒绝</div>
-        <div v-else>待审批</div>
-      </div>
-    </div>
+    </template>
   </div>
   <div class="pager">
     <el-pagination
@@ -143,14 +55,20 @@
 </template>
 
 <script>
-import { ref, getCurrentInstance } from 'vue'
+import { ref, getCurrentInstance, onMounted, onBeforeUnmount } from 'vue'
 
 export default {
   setup () {
+    /** @type {{proxy:import("../../main").LocalComponentInstance}} 访问 app 实例上挂载的各插件 */
     const { proxy } = getCurrentInstance()
     const roomList = proxy.$store.getters.roomList
     const weekBeginDateStr = '2022-02-21' // 学期起始时间
     const weekBeginDate = new Date(weekBeginDateStr)
+    const totalAppointList = ref([])
+    const monDate = new Date(weekBeginDate.getTime())
+    const sunDate = new Date(monDate.getTime() + 6 * 1000 * 3600 * 24)
+    const roomId = ref(1)
+    const currentPage = ref(1)
     const roomWeekArr = ref([
       [], [], [], [], [], []
     ])
@@ -164,15 +82,12 @@ export default {
             schoolId: null,
             userNote: null,
             conductor: null,
+            execDate: null,
             status: null
           })
         }
       }
     }
-    var monDate = new Date(weekBeginDate.getTime())
-    var sunDate = new Date(monDate.getTime() + 6 * 1000 * 3600 * 24)
-    const roomId = ref(1)
-    const currentPage = ref(1)
     const getDateString = (dateObj) => {
       const Y = dateObj.getFullYear() + '-'
       const M = (dateObj.getMonth() + 1 < 10 ? '0' + (dateObj.getMonth() + 1) : dateObj.getMonth() + 1) + '-'
@@ -180,66 +95,80 @@ export default {
       return Y + M + D
     }
     // 获取某房间一周预约记录
-    const getRoomAppList = async (monDateObj, sunDateObj) => {
+    const getRoomAppList = async () => {
       initWeekArr()
-      const monDatStr = getDateString(monDateObj)
-      const sunDatStr = getDateString(sunDateObj)
-      const { totalPages } = await proxy.$api.appointmentList({
-        current: 0,
-        limit: 10
-      })
-      // 逐页获取预约记录
-      for (let i = 0; i < totalPages; i++) {
-        const { items } = await proxy.$api.appointmentList({
-          current: i,
-          limit: 10
-        })
-        for (let j = 0; j < items.length; j++) {
-          if (
-            items[j].roomId === roomId.value &&
-            items[j].execDate >= monDatStr &&
-            items[j].execDate <= sunDatStr
-          ) {
-            var tmpday = new Date(items[j].execDate)
-            var tmpWeekDay = tmpday.getDay()
-            var tmpTime = items[j].begin
-            if (tmpWeekDay === 0) {
-              tmpWeekDay = 6
-            } else {
-              tmpWeekDay--
-            }
-            roomWeekArr.value[tmpTime - 1][tmpWeekDay] = {
-              id: items[j].id,
-              username: items[j].username,
-              schoolId: items[j].schoolId,
-              userNote: items[j].userNote,
-              conductor: items[j].conductor,
-              execDate: items[j].execDate,
-              status: items[j].status
-            }
+      const monDatStr = getDateString(monDate)
+      const sunDatStr = getDateString(sunDate)
+      for (var item of totalAppointList.value) {
+        if (
+          item.roomId === roomId.value &&
+          item.execDate >= monDatStr &&
+          item.execDate <= sunDatStr
+        ) {
+          var tmpDay = new Date(item.execDate)
+          var tmpWeekDay = tmpDay.getDay()
+          var tmpTime = item.begin
+          if (tmpWeekDay === 0) {
+            tmpWeekDay = 6
+          } else {
+            tmpWeekDay--
+          }
+          roomWeekArr.value[tmpTime - 1][tmpWeekDay] = {
+            id: item.id,
+            username: item.username,
+            schoolId: item.schoolId,
+            userNote: item.userNote,
+            conductor: item.conductor,
+            execDate: item.execDate,
+            status: item.status
           }
         }
       }
     }
+    // 获取所有预约记录
+    const getTotalAppointList = async () => {
+      const { totalPages } = await proxy.$api.appointmentList({
+        current: 0,
+        limit: 10
+      })
+      const { items } = await proxy.$api.appointmentList({
+        current: 0,
+        limit: 10 * totalPages
+      })
+      totalAppointList.value = items
+      getRoomAppList()
+    }
     // 选择房间
-    const handleSelectChange = async () => {
-      getRoomAppList(monDate, sunDate)
+    const handleSelectChange = () => {
+      getRoomAppList()
     }
     // 选择周次
-    const handlePageChange = async (current) => {
+    const handlePageChange = (current) => {
       currentPage.value = current
-      monDate.setTime(weekBeginDate.getTime() +
-      (currentPage.value - 1) * 7 * 1000 * 3600 * 24)
+      monDate.setTime(weekBeginDate.getTime() + (currentPage.value - 1) * 7 * 1000 * 3600 * 24)
       sunDate.setTime(monDate.getTime() + 6 * 1000 * 3600 * 24)
-      getRoomAppList(monDate, sunDate)
+      getRoomAppList()
     }
+    // 单击订单
     const handleClick = (orderNum) => {
       const newWindowPath = proxy.$router.resolve({
         path: `/appointmentItem/${orderNum}`
       })
       window.open(newWindowPath.href, '_target')
     }
-    getRoomAppList(monDate, sunDate)
+    initWeekArr()
+    // ajax 轮询实时更新订单
+    const timeRef = ref(0)
+    onMounted(() => {
+      getTotalAppointList()
+      timeRef.value = setInterval(() => {
+        getTotalAppointList()
+      }, 1000 * 30)
+    })
+    // 组件销毁前，清除定时器，避免内存泄漏
+    onBeforeUnmount(() => {
+      clearInterval(timeRef.value)
+    })
     return {
       roomId,
       roomList,
@@ -262,9 +191,9 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
-  }
-  .m-2{
+    .m-2{
     margin-left: 40px;
+    }
   }
   .prompt {
     padding-top: 15px;
@@ -274,8 +203,6 @@ export default {
     background-color: #fff;
   }
   .calendar {
-    // width: 280px;
-    // height: 320px;
     padding: 30px;
     display: grid;
     background-color: #fff;
@@ -296,6 +223,11 @@ export default {
         padding-left: 3px;
       }
     }
+    .line-limit-length {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
     .table-header {
       font-size: 18px;
       padding-top: 45px;
@@ -308,11 +240,6 @@ export default {
     display: flex;
     justify-content: flex-start;
     align-items: center;
-  }
-  .line-limit-length {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap; //文本不换行，这样超出一行的部分被截取，显示...
   }
 }
 </style>

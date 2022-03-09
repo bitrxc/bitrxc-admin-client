15abf61f78524d4170dacc9653a43b5cf635797a
jingkaimori <jingkaimori@gmail.com>
2022-03-09 09:24:33 +0800

    [hack] remove several status from frontend

--- src/views/admin/AppointmentItem.vue
+++ src/views/admin/AppointmentItem.vue
@@ -86,10 +86,10 @@ export default {
     const statusOptions = [
       { value: 'new', label: '待审核' },
       { value: 'receive', label: '已批准' },
-      { value: 'signed', label: '已签到' },
-      { value: 'illegal', label: '未签退' },
-      { value: 'finish', label: '已签退' },
-      { value: 'missed', label: '爽约' },
+      // { value: 'signed', label: '已签到' },
+      // { value: 'illegal', label: '未签退' },
+      // { value: 'finish', label: '已签退' },
+      // { value: 'missed', label: '爽约' },
       { value: 'reject', label: '已驳回' },
       { value: 'cancel', label: '用户撤回' }
     ]

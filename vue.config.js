7375a222fdd1820820b93247d98463212117e717
jingkaimori <jingkaimori@gmail.com>
2022-03-17 16:45:21 +0800

    [feat] enable pre-compiled gzip

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,16 @@ module.exports = {
   configureWebpack: {
     devtool: process.env.NODE_ENV === 'development' ? 'source-map' : undefined
   },
+  pluginOptions: {
+    compression: {
+      gzip: {
+        filename: '[file].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8
+      }
+    }
+  },
   devServer: {
     proxy: {
       '/api': {

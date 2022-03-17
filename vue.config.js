5d365beac6ef3ad5ac3cb1af7b30dda363601ffb
jingkaimori <jingkaimori@gmail.com>
2022-03-17 21:15:56 +0800

    [feat] alter gzip dependency

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,18 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+
 module.exports = {
-  // 开发环境时启动 source-map
   configureWebpack: {
-    devtool: process.env.NODE_ENV === 'development' ? 'source-map' : undefined
-  },
-  pluginOptions: {
-    compression: {
-      gzip: {
+    // 本意是仅在开发环境生成 source-map 。由于 package.json 中定义的 vue-cli-service build 指令会将 NODE_ENV 覆盖，因此下方语句没用
+    // devtool: process.env.NODE_ENV === 'development' ? 'source-map' : undefined,
+    plugins: [
+      // 压缩文件以启用静态gzip
+      new CompressionPlugin({
         filename: '[file].gz[query]',
         algorithm: 'gzip',
-        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        include: /\.(js|css|html|ttf|ico|svg|json|map)(\?.*)?$/i,
         minRatio: 0.8
-      }
-    }
+      })
+    ]
   },
   devServer: {
     proxy: {
